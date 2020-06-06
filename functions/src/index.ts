da9032dc675580e7aa627a39402a8ef129ad1c5b
diaphragm <diaphragm@9kv.org>
2020-06-07 02:14:28 +0900

    twitterへの投稿機能ブラッシュアップ

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,7 +13,7 @@ admin.initializeApp()
 
 
 // export const onCreateBellTrigger = functions
-//   .region('asia-northeast1')
+//   .region(REGION)
 //   .firestore
 //   .document('bells/{bellId}')
 //   .onCreate((snap, context) => {
@@ -29,7 +29,7 @@ admin.initializeApp()
 //   })
 
 // export const onUpdateBellTrigger = functions
-//   .region('asia-northeast1')
+//   .region(REGION)
 //   .firestore
 //   .document('bells/{bellId}')
 //   .onUpdate((change, context) => {
@@ -44,44 +44,96 @@ admin.initializeApp()
 //     })
 // })
 
-
-export const printenv = functions.https.onRequest((req, res) => {
-  console.log('functions.config()', functions.config())
-  console.log('process.env', process.env)
-  res.send("Please check firebase dashboard.")
-})
-
-const client = new Twitter(functions.config().twitter)
-const tweet = (status: String) => {
-  return client.post('statuses/update', { status })
+interface Bell {
+  [key: string]: string | FirebaseFirestore.Timestamp,
 }
 
-const genTweetUrl = (tweet: Twitter.ResponseData): String => {
+const REGION = 'asia-northeast1'
+const RETWEET_INTERVAL = 1 * 60 // seconds
+const BELL_ATTR_LIST = ['place', 'password', 'note', 'region', 'silencedAt']
+
+const TwitterClient = new Twitter(functions.config().twitter)
+
+const genTweetUrl = (tweet: Twitter.ResponseData): string => {
   return `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
 }
 
+const bellDiff = (newBell: FirebaseFirestore.DocumentData, oldBell: FirebaseFirestore.DocumentData) => {
+  const ret: Bell = {}
+  BELL_ATTR_LIST.forEach((attr) => {
+    if (newBell[attr] !== oldBell[attr]) {
+      ret[attr] = newBell[attr]
+    }
+  })
+  if (Object.keys(ret).length > 0) {
+    return ret
+  } else {
+    return null
+  }
+}
 
-export const dev = functions.https.onRequest((req, res) => {
-  tweet('test')
-  res.send('tweeted')
-})
-
-export const onRingedBellTritter = functions
-  .region('asia-northeast1')
+export const onCreatedBellTritter = functions
+  .region(REGION)
   .firestore
   .document('bells/{bellId}')
   .onCreate((snap, context) => {
     const id = snap.id
     const {place, note} = snap.data() || {}
     const url = `${functions.config().app.base_url}${id}`
-    const message = `${place}で鐘を鳴らしています。 ${url}\n${note}`
+    const message = `${place}で鐘を鳴らしています ${url}\n${note}`
 
-    tweet(message).then(tweet => {
-      const url = genTweetUrl(tweet)
+    TwitterClient.post('statuses/update', {
+      status: message,
+    }).then(tweet => {
+      const tweetUrl = genTweetUrl(tweet)
       return admin.firestore().collection('bells').doc(snap.id).update({
-        tweetUrl: url
+        tweetUrl: tweetUrl
       })
     }).catch(error => {
       console.error(error)
     })
+
+    return 0
+  })
+
+export const onUpdatedBellTrigger = functions
+  .region(REGION)
+  .firestore
+  .document('bells/{bellId}')
+  .onUpdate((change, context) => {
+    const newData = change.after.data()
+    const oldData = change.before.data()
+
+    const diff = bellDiff(newData, oldData)
+    const interval = newData.updatedAt.seconds - oldData.updatedAt.seconds
+
+    if (diff) {
+      if (diff.silencedAt || interval > RETWEET_INTERVAL) {
+        const id = change.after.id
+        const { place, note, tweetUrl } = newData || {}
+        const url = `${functions.config().app.base_url}${id}`
+        const message = diff.silencedAt ?
+          `【終了】 募集は終了しました` :
+          `【更新】 ${place}で鐘を鳴らしています。 ${url}\n${note}`
+        const status = `${message} ${tweetUrl}`
+        const tweetId = tweetUrl.match(/\d+$/)[0]
+
+        TwitterClient.post('statuses/update', {
+          status: status,
+          in_reply_to_status_id: tweetId
+        }).catch(error => {
+          console.error(error)
+        })
+      }
+    }
+
+    return 0
+  })
+
+// debug
+export const printenv = functions.region(REGION)
+  .https.onRequest((req, res) => {
+    console.log('functions.config()', functions.config())
+    console.log('process.env', process.env)
+    res.send("Please check firebase dashboard.")
   })
