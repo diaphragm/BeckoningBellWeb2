f5857b7bea8359ab0da90336cea69d883142f486
diaphragm <diaphragm@9kv.org>
2020-06-06 22:30:33 +0900

    configを修正、環境ごとに切り出した

---
+++ env.config.js
@@ -0,0 +1,17 @@
+export default  {
+  staging: {
+    firebaseConfig: {
+      apiKey: "AIzaSyCGVt3-mkrv-TNF-OSrccNWGXUUNsVHIp8",
+      authDomain: "beckoningbellweb-staging.firebaseapp.com",
+      databaseURL: "https://beckoningbellweb-staging.firebaseio.com",
+      projectId: "beckoningbellweb-staging",
+      storageBucket: "beckoningbellweb-staging.appspot.com",
+      messagingSenderId: "757753230186",
+      appId: "1:757753230186:web:e0333edaff2207cecc6b13",
+      measurementId: "G-S88G20F0TG",
+    },
+    baseUrl: 'https://beckoningbellweb-staging.web.app/'
+  },
+  production: {
+  }
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
+import config from './env.config.js'
 import colors from 'vuetify/es5/util/colors'
+
+const NODE_ENV = process.env.NODE_ENV
 
 export default {
   mode: 'spa',
@@ -51,16 +54,7 @@ export default {
     [
       '@nuxtjs/firebase',
       {
-        config: {
-          apiKey: "AIzaSyCGVt3-mkrv-TNF-OSrccNWGXUUNsVHIp8",
-          authDomain: "beckoningbellweb-staging.firebaseapp.com",
-          databaseURL: "https://beckoningbellweb-staging.firebaseio.com",
-          projectId: "beckoningbellweb-staging",
-          storageBucket: "beckoningbellweb-staging.appspot.com",
-          messagingSenderId: "757753230186",
-          appId: "1:757753230186:web:e0333edaff2207cecc6b13",
-          measurementId: "G-S88G20F0TG",
-        },
+        config: config[NODE_ENV].firebaseConfig,
         onFirebaseHosting: true,
         services: {
           auth: true,
