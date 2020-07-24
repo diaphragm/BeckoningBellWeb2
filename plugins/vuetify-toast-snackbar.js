3de002a604995f63b2379f29c1fe181ab2d8e2e8
diaphragm <diaphragm@9kv.org>
2020-07-25 02:08:45 +0900

    update libraries

--- plugins/vuetify-toast-snackbar.js
+++ plugins/vuetify-toast-snackbar.js
@@ -1,16 +1,30 @@
+// import Vue from 'vue'
+// import Vuetify, { VSnackbar, VBtn, VIcon } from 'vuetify/lib'
+// import VuetifyToast from 'vuetify-toast-snackbar'
+
+// Vue.use(Vuetify, {
+//   components: {
+//     VSnackbar,
+//     VBtn,
+//     VIcon
+//   }
+// })
+
+// Vue.use(VuetifyToast, {
+//   x: null,
+//   y: 'top'
+// })
+
+
+// solve vuetify-toast-snackbar probrem
+// https://github.com/eolant/vuetify-toast-snackbar/issues/42#issuecomment-646496012
 import Vue from 'vue'
-import Vuetify, { VSnackbar, VBtn, VIcon } from 'vuetify/lib'
 import VuetifyToast from 'vuetify-toast-snackbar'
 
-Vue.use(Vuetify, {
-  components: {
-    VSnackbar,
-    VBtn,
-    VIcon
-  }
-})
-
-Vue.use(VuetifyToast, {
-  x: null,
-  y: 'top'
-})
+export default ({$vuetify}) => {
+  Vue.use(VuetifyToast, {
+    $vuetify,
+    x: null,
+    y: 'top'
+  })
+}
